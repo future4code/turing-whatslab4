d6e4bf651581cb04bbf68534471f6e76379e9616
Alice Souza <alicepsouzaba1998@gmail.com>
2020-06-19 14:45:58 -0300

    Conteúdo do return adicionado em Chat.js

--- whatslab/src/componentes/chat.js
+++ whatslab/src/componentes/chat.js
@@ -2,17 +2,35 @@ import React from 'react'
 
 export class Chat extends React.Component {
   state = {
-    usuario: " ",
-    mensagem: ""
+    usuario: '',
+    mensagem: ''
   }
 
-  onChangeUsuario = event => {
+  onChangeUsuario = (event) => {
     this.setState({usuario: event.target.value})
   }
 
-  onClickBotaoPegaMensagens = () => {
-    console.log(this.state.usuario)
-    this.setState({ usuario: "" })
+  onChangeMensagem = (event) => {
+    this.setState({mensagem: event.target.value})
   }
 
+  onClickBotaoPegaMensagens = () => {
+    
+  }
+
+  render(){
+    return(
+      <div className='Chat'>
+        <div className='saida'>
+          <p><strong></strong></p>
+          <p></p>
+        </div>
+        <div className='entrada'>
+          <input onChange={this.onChangeUsuario} value={this.state.usuario} placeholder='Usuário'></input>
+          <input onChange={this.onChangeMensagem} value={this.state.mensagem} placeholder='Mensagem'></input>
+          <button onClick={this.onClickBotaoPegaMensagens}>Enviar</button>
+        </div>
+      </div>
+    )
+  }
 }
