4e86b687651582264a6927e3fae4302de07253f7
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-19 17:17:32 -0300

    Nome do componente alterado

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Chat from './componentes/chat'
+import Chat from './componentes/Chat'
 
 function App() {
   return (
--- whatslab/src/componentes/chat.js
+++ whatslab/src/componentes/chat.js
@@ -2,17 +2,52 @@ import React from 'react'
 
 export class Chat extends React.Component {
   state = {
-    usuario: " ",
-    mensagem: ""
+    chat:[
+      {
+      usuario: '',
+      mensagem: ''
+      }
+    ],
+    valorUsuario: "",
+    valorMensagem:""
   }
 
-  onChangeUsuario = event => {
-    this.setState({usuario: event.target.value})
-  }
 
   onClickBotaoPegaMensagens = () => {
-    console.log(this.state.usuario)
-    this.setState({ usuario: "" })
+    const newChat = {
+      usuario: this.state.valorUsuario,
+      mensagem: this.state.valorMensagem
+    }
+    const novoChat = [...this.state.chat, newChat]
+
+    this.setState({chat: novoChat })
+  
+  }
+  onChangeUsuario = (event) => {
+    this.setState({valorUsuario: event.target.value})
   }
 
+  onChangeMensagem = (event) => {
+    this.setState({valorMensagem: event.target.value})
+  }
+
+  render(){
+    const renderizaChat = this.state.chat.map((msg)=>{
+      return (
+      <p>{msg.usuario}: {msg.mensagem}</p>
+      )
+    })
+    return(
+      <div className='Chat'>
+        <div className='saida'>
+          <div>{renderizaChat}</div>
+        </div>
+        <div className='entrada'>
+          <input onChange={this.onChangeUsuario} value={this.state.usuario} placeholder='Usuário'></input>
+          <input onChange={this.onChangeMensagem} value={this.state.mensagem} placeholder='Mensagem'></input>
+          <button onClick={this.onClickBotaoPegaMensagens}>Enviar</button>
+        </div>
+      </div>
+    )
+  }
 }
