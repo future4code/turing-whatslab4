dd8ece3de8424bc8e92ffd27eb62cf093d35bdba
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-19 12:45:07 -0300

    Feito os estados do componente Chat e a função de pegar a mensagem. Só está exibindo no console.

--- whatslab/src/componentes/chat.js
+++ whatslab/src/componentes/chat.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
 export class Chat extends React.Component {
-  
+  state = {
+    remetente: " ",
+    usuario: " ",
+  }
+
+  onChangeRemetente = event => {
+    this.setState({remetente: event.target.value})
+  }
+
+  onChangeUsuario = event => {
+    this.setState({usuario: event.target.value})
+  }
+
+  onClickBotaoPegaMensagens = () => {
+    console.log(this.state.remetente)
+    console.log(this.state.usuario)
+    this.setState({ remetente: "", usuario: "" })
+  }
+
 }
