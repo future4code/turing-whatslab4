1daa14cf11f273f04fef5caa106bbfe4ab36e90b
laisflaviosilva <laisflavio.silva@gmail.com>
2020-06-19 16:24:18 -0300

    Fiz as alterações na master

--- whatslab/src/componentes/chat.js
+++ whatslab/src/componentes/chat.js
@@ -2,28 +2,45 @@ import React from 'react'
 
 export class Chat extends React.Component {
   state = {
-    usuario: '',
-    mensagem: ''
+    chat:[
+      {
+      usuario: '',
+      mensagem: ''
+      }
+    ],
+    valorUsuario: "",
+    valorMensagem:""
   }
 
+
+  onClickBotaoPegaMensagens = () => {
+    const newChat = {
+      usuario: this.state.valorUsuario,
+      mensagem: this.state.valorMensagem
+    }
+    const novoChat = [...this.state.chat, newChat]
+
+    this.setState({chat: novoChat })
+  
+  }
   onChangeUsuario = (event) => {
-    this.setState({usuario: event.target.value})
+    this.setState({valorUsuario: event.target.value})
   }
 
   onChangeMensagem = (event) => {
-    this.setState({mensagem: event.target.value})
-  }
-
-  onClickBotaoPegaMensagens = () => {
-    
+    this.setState({valorMensagem: event.target.value})
   }
 
   render(){
+    const renderizaChat = this.state.chat.map((msg)=>{
+      return (
+      <p>{msg.usuario}: {msg.mensagem}</p>
+      )
+    })
     return(
       <div className='Chat'>
         <div className='saida'>
-          <p><strong></strong></p>
-          <p></p>
+          <div>{renderizaChat}</div>
         </div>
         <div className='entrada'>
           <input onChange={this.onChangeUsuario} value={this.state.usuario} placeholder='Usuário'></input>
